505d969ba53708665df6905e3203fa0b417707f7
agustin donoso morales <adm@stiga.es>
2017-12-23 01:18:03 +0100

    activityIndicator

--- ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
+++ ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
@@ -6,36 +6,85 @@ namespace ForeignExchange.ViewModels
     using Models;
     using System;
     using System.Collections.ObjectModel;
+    using System.ComponentModel;
     using System.Windows.Input;
 
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Attributes
+        bool _isRunning;
+        string _result;
+        #endregion
+
 
         #region Propiedades
         public string Amount { get; set; }
         public ObservableCollection<Rate> Rates { get; set; }
         public Rate SourceRate { get; set; }
         public Rate TargetRate { get; set; }
-        public bool IsRunning { get; set; }
+        public bool IsRunning {
+            get {
+                return _isRunning;
+            }
+            set {
+                if(_isRunning !=value)
+                {
+                    _isRunning = value;
+                    PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(nameof(IsRunning)));
+                }
+            }
+        }
         public bool IsEnabled { get; set; }
-        public String Result { get; set; }
-
+        public String Result
+        {
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Result)));
+                }
+            }
+        }
         #endregion
 
         #region Commands
-        public ICommand ConvertCommand { get {
+        public ICommand ConvertCommand {
+            get {
                 return new RelayCommand(Convert);
             }
         }
+
+      
 
         private void Convert()
         {
             throw new NotImplementedException();
         }
         #endregion
+
+
         public MainViewModel()
         {
-
+            LoadRates(); 
+            
         }
+
+        #region Methods
+
+        void LoadRates()
+        {
+            IsRunning = true;
+            Result = "Loading rates...";
+        }
+        #endregion
     }
 }
