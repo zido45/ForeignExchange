24c5878b4b3cfbdff1a13d1e633aac1a259c9619
agustin donoso morales <adm@stiga.es>
2017-12-24 16:45:06 +0100

    servicio consumido y añadido boton sin evento

--- ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
+++ ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
@@ -4,10 +4,14 @@ namespace ForeignExchange.ViewModels
 {
     using GalaSoft.MvvmLight.Command;
     using Models;
+    using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.ComponentModel;
+    using System.Net.Http;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
     public class MainViewModel : INotifyPropertyChanged
     {
@@ -18,12 +22,31 @@ namespace ForeignExchange.ViewModels
         #region Attributes
         bool _isRunning;
         string _result;
+        bool _isenabled;
+        ObservableCollection<Rate> _rates;
         #endregion
 
 
         #region Propiedades
         public string Amount { get; set; }
-        public ObservableCollection<Rate> Rates { get; set; }
+        public ObservableCollection<Rate> Rates {
+
+            get
+            {
+                return _rates;
+            }
+            set
+            {
+                if (_rates != value)
+                {
+                    _rates = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Rates)));
+                }
+            }
+
+
+
+        }
         public Rate SourceRate { get; set; }
         public Rate TargetRate { get; set; }
         public bool IsRunning {
@@ -38,7 +61,23 @@ namespace ForeignExchange.ViewModels
                 }
             }
         }
-        public bool IsEnabled { get; set; }
+        public bool IsEnabled {
+            get
+            {
+                return _isenabled;
+            }
+            set
+            {
+                if (_isenabled != value)
+                {
+                    _isenabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsEnabled)));
+                }
+            }
+
+        }
+
+
         public String Result
         {
             get
@@ -65,9 +104,47 @@ namespace ForeignExchange.ViewModels
 
       
 
-        private void Convert()
+        async  private void Convert()
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(Amount))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a value in amount",
+                    "Accept");
+                return;
+            }
+
+            if (!decimal.TryParse(Amount, out decimal amount))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                "Error",
+                "You must enter a numeric value in amount",
+                "Accept");
+                return;
+            }
+
+            if (SourceRate==null)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must select a source rate",
+                    "Accept");
+                return;
+            }
+
+            if (TargetRate == null)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must select a target rate",
+                    "Accept");
+                return;
+            }
+
+            var amountConverted = amount / (decimal)SourceRate.TaxRate * (decimal)TargetRate.TaxRate;
+            
+            Result = string.Format("{0} {1:C2} ={2} {3:c2} ",SourceRate.Code,amount,TargetRate.Code,amountConverted);
         }
         #endregion
 
@@ -80,10 +157,39 @@ namespace ForeignExchange.ViewModels
 
         #region Methods
 
-        void LoadRates()
+        async void LoadRates()
         {
             IsRunning = true;
             Result = "Loading rates...";
+
+            try
+            {
+                var client = new HttpClient(); //instanciamos el cliente para consumir
+                client.BaseAddress = new Uri("http://apiexchangerates.azurewebsites.net"); //definimos la url base del servicio
+                var controller = "/api/rates"; //resto de la direccion del servicio
+                //hasta hora solo hemos puesto las balas a la pistola pero aun no hemos disparado
+                var response = await client.GetAsync(controller); //aqui disparamos
+                var result = await response.Content.ReadAsStringAsync();
+
+                if(!response.IsSuccessStatusCode)
+                {
+                    IsRunning = false;
+                    Result = result;
+                }
+                //si si hay resultado hay que convertir el string al objeto deseado en este caso una lista de rates
+                var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+                // hay que convertirlo en una observable collection
+                Rates = new ObservableCollection<Rate>(rates);
+                IsRunning = false;
+                Result = "Ready to convert";
+                IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+
+                IsRunning = false;
+                Result = ex.Message;
+            }
         }
         #endregion
     }
